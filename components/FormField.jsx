872cd5f14a2363a94f12f0ac8bf5dc2d3a378c9b
OsazeUzama <Osazeuzama@gmail.com>
2024-09-23 09:42:16 +0100

    Implemented the home-page

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -15,7 +15,7 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
 
       <View className=" border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
         <TextInput 
-          className=" flex-1 text-white text-base font-psemibold"
+          className="flex-1 text-white text-base font-psemibold"
           value={value}
           placeholder={placeholder}
           placeholderTextColor="#7b7b8b"
