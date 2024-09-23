f096ba630153b317083dc42872bbeb9f37e6433a
OsazeUzama <Osazeuzama@gmail.com>
2024-09-23 16:20:11 +0100

    Implemented Profile

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,9 +9,11 @@ import EmptyState from '../../components/EmptyState'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 
 const Home = () => {
+  const { user } = useGlobalContext();
 
   const { data: posts, refetch } = useAppwrite(getAllPosts);
 
@@ -38,10 +40,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className='font-pmedium text-sm text-gray-100'>
-                  Welcome back
+                  Welcome back,
                 </Text>
                 <Text className='text-2xl font-psemibold text-white'>
-                  Osaze
+                  {user?.username}
                 </Text>
               </View>
 
